6b785a5fbd0b53201fb27a01a9a67a473e0c2c8a
anish1A1 <alloozpat87@gmail.com>
2024-11-10 08:16:51 +0545

    Correct way to use an object and updating its value

--- src/UpdatingObjects/Form.jsx
+++ src/UpdatingObjects/Form.jsx
@@ -9,36 +9,53 @@ export default function Form() {
     });
 
     function handleFirstnameChange(e){
-        person.firstName = e.target.value;
+        setPerson({  // first create a object
+            ...person, // then spread the existing object 
+            firstName: e.target.value // then update the value of the property
+
+        }
+        )
     }
 
     function handleLastnameChange(e) {
-        person.lastName = e.target.value;
+       setPerson({
+            ...person,
+            lastName: e.target.value
+       })
+       
     }
     function handleEmailChange(e) {
-        person.email = e.target.value;
+       setPerson({
+        ...person,
+        email: e.target.value
+       })
     }
+
 
     return (
         <>
             <label className="font-black">
                 First Name: {person.firstName}
            
-            <input type="text" value={person.firstName}
+            <input type="text" value={person.firstName}  name="FirstName"
                 onChange={handleFirstnameChange} className="  bg-transparent"
             />
              </label>
 
             <label htmlFor="" >
                 Last Name: {person.lastName}
-            <input type="text" value={person.lastName} onChange={handleLastnameChange}
+            <input type="text" value={person.lastName} name="LastName"
+             onChange={handleLastnameChange}
             className="  bg-transparent"/>
             </label>
 
             <label htmlFor="" >
                 Email: {person.email}
-            <input type="text" value={person.email} onChange={handleEmailChange} className="  bg-transparent"/>
+            <input type="text" value={person.email} name="email"
+             onChange={handleEmailChange} className="  bg-transparent"/>
             </label>
         </>
+        // Note that the ... spread syntax is “shallow”—it only copies things one level deep. 
+        //This makes it fast, but it also means that if you want to update a nested property, you’ll have to use it more than once.
     );
 }
